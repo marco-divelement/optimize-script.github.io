eabe7f3d019c0970b684790da767c7eb6c141294
Marco Garcia <macbookpro@MacBook-Pro-de-Macbook.local>
2023-01-10 01:54:32 -0600

    Push styles

--- docs/scripts/mobile-banner.js
+++ docs/scripts/mobile-banner.js
@@ -3,7 +3,7 @@ let mobileBanner = setInterval(() => {
         clearInterval(mobileBanner)
 
         console.log("Variant 2")
-        console.log("Version 2.9")
+        console.log("Version 2.10")
 
         let eventVar = "desktop"
 
